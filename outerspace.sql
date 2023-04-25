1436612e07d1d29615320904336315b51c8536d6
Kenny Burgher <kenny.burgher@gmail.com>
2023-04-25 16:32:49 -0700

    refactoring

--- outerspace.sql
+++ outerspace.sql
@@ -1,24 +1,24 @@
-CREATE TABLE star(
+CREATE TABLE stars(
     star_name VARCHAR(50) PRIMARY KEY,
     temp_in_kelvin INT
 );
 
-INSERT INTO star
+INSERT INTO stars
     (star_name, temp_in_kelvin)
 VALUES
     ('The Sun', 5800),
     ('Proxima Centauri', 3042),
     ('Gliese 876', 3192);
 
-CREATE TABLE planet(
+CREATE TABLE planets(
     planet_name VARCHAR(50) PRIMARY KEY,
-    orbital_period_yrs NUMERIC(5,3) NOT NULL
+    orbital_period_yrs FLOAT NOT NULL
         CHECK (orbital_period_yrs > 0),
-    star_orbited VARCHAR(50) NOT NULL REFERENCES star
+    star_name VARCHAR(50) NOT NULL REFERENCES star
 );
 
-INSERT INTO planet
-    (planet_name, orbital_period_yrs, star_orbited)
+INSERT INTO planets
+    (planet_name, orbital_period_yrs, star_name)
 VALUES
     ( 'Earth', 1.00, 'The Sun'),
     ('Mars', 1.822, 'The Sun'),
@@ -26,13 +26,13 @@ VALUES
     ('Proxima Centauri b', 0.03, 'Proxima Centauri'),
     ('Gliese 876 b', 0.236, 'Gliese 876');
 
-CREATE TABLE moon(
+CREATE TABLE moons(
     moon_name VARCHAR(50) PRIMARY KEY,
-    planet_orbited VARCHAR(50) NOT NULL REFERENCES planet
+    planet_name VARCHAR(50) NOT NULL REFERENCES planet
 );
 
-INSERT INTO moon
-    (moon_name, planet_orbited)
+INSERT INTO moons
+    (moon_name, planet_name)
 VALUES
     ('The Moon', 'Earth'),
     ('Phobos', 'Mars'),
@@ -41,11 +41,11 @@ VALUES
 -- for each planet:
     -- its name, the star it revolves around,
     -- and the numbers of moons (if any) it has
-SELECT planet_name, star_orbited, count(moon_name) AS num_moons
-FROM planet AS p
-    JOIN star AS s
-        ON p.star_orbited = s.star_name
-    LEFT JOIN moon AS m
-        ON m.planet_orbited = p.planet_name
+SELECT planet_name, star_name, count(moon_name) AS num_moons
+FROM planets
+    JOIN stars
+        USING (star_name)
+    LEFT JOIN moons
+        USING (planet_name)
 GROUP BY planet_name
 ORDER BY planet_name;
